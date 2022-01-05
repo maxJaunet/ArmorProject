8e10c388a5df6bafe7ee1578737658cbdbb382a0
maxJaunet <86613008+MaxJ-International@users.noreply.github.com>
2022-01-05 15:21:46 +0100

    feat: setting up functions.php and include bootstrap 5 scripts and styles

---
+++ app/bedrock/web/app/themes/armor_theme/functions.php
@@ -0,0 +1,16 @@
+<?php
+  function myTheme_register_assets(){
+    wp_register_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css');
+    wp_register_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js', [], null, true);
+    wp_enqueue_style('bootstrap');
+    wp_enqueue_script('bootstrap', [], false, true);
+  };
+
+  function myTheme_supports(){
+    add_theme_support('title-tag');
+  };
+
+  add_action('after_setup_theme', 'myTheme_supports');
+  add_action('wp_enqueue_scripts', 'myTheme_register_assets');
+
+?>
