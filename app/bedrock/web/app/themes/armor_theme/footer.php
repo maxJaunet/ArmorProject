5b40c3a5c96d118ea43fb3f6433247dea24eaf92
maxJaunet <86613008+MaxJ-International@users.noreply.github.com>
2022-01-05 18:19:13 +0100

    feat: creating footer

--- app/bedrock/web/app/themes/armor_theme/footer.php
+++ app/bedrock/web/app/themes/armor_theme/footer.php
@@ -1,6 +1,29 @@
   
-  </div>
- <?php wp_footer(); ?>
+  <footer class="footer container p-5">
+    <div class="row justify-content-between">
+      <div class="col-4">
+        <h2>logo here</h2>
+        <p class="footer-subText">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa quas doloribus esse.
+        </p>
+      </div>
+      <div class="col-4">
+        <h2>logo here</h2>
+        <p class="footer-subText">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        </p>
+      </div>
+    </div>
+    <div class="row justify-content-between">
+      <nav class="nav-list">
+        <ul>
+          <li><a class="nav-item" href="#">Mentions légales</a></li>
+          <li><a class="nav-item" href="#">Protection des données</a></li>
+          <li><a class="nav-item" href="#">Gestion des cookies</a></li>
+        </ul>
+      </nav>
+    </div>
+  </footer>
 </body>
 
 </html>
